edf1d62369a89a0427e0878f742c38cc8bed3690
yoieh <yoieh@live.se>
2022-12-14 20:50:38 +0100

    moved paning to keyboard

--- Assets/Scripts/PanAndZoom.cs
+++ Assets/Scripts/PanAndZoom.cs
@@ -25,9 +25,10 @@ public class PanAndZoom : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float z = inputProvider.GetAxisValue(2);
+
         float x = inputProvider.GetAxisValue(0);
         float y = inputProvider.GetAxisValue(1);
-        float z = inputProvider.GetAxisValue(2);
 
         if (x != 0 || y != 0)
         {
@@ -51,43 +52,12 @@ public class PanAndZoom : MonoBehaviour
         );
     }
 
-
-    public Vector2 PanDirection(float x, float y)
-    {
-        Vector2 direction = Vector2.zero;
-
-        if (y >= Screen.height * .95f)
-        {
-            direction.y += 1;
-        }
-        else if (y <= Screen.height * .05f)
-        {
-            direction.y -= 1;
-        }
-
-        if (x >= Screen.width * .95f)
-        {
-            direction.x += 1;
-        }
-        else if (x <= Screen.width * .05f)
-        {
-            direction.x -= 1;
-        }
-
-        return direction;
-    }
-
-
     public void PanScreen(float x, float y)
     {
-        Vector2 direction = PanDirection(x, y);
         cameraTransform.position = Vector3.Lerp(
             cameraTransform.position,
-            cameraTransform.position + (Vector3)direction,
-            panSpeed * Time.deltaTime
+            cameraTransform.position + (Vector3.right * x + Vector3.up * y),
+            panSpeed * virtualCamera.m_Lens.OrthographicSize * Time.deltaTime
         );
-
-
-
     }
 }
